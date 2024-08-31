68d021b2b39e62275df23b83f73011caf39c2bab
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-08-31 13:01:55 -0300

    add an method that register the items on forge

--- src/main/java/net/vlade/tutorialmod/item/ModItems.java
+++ src/main/java/net/vlade/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.vlade.tutorialmod.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.vlade.tutorialmod.TutorialMod;
@@ -8,4 +9,12 @@ import net.vlade.tutorialmod.TutorialMod;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+
+
 }
