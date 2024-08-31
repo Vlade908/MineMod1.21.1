bb6046578f27400827bdb84f59df37806699b130
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-08-31 12:58:32 -0300

    add a item folder and a item java class

--- src/main/java/net/vlade/tutorialmod/item/ModItems.java
+++ src/main/java/net/vlade/tutorialmod/item/ModItems.java
@@ -1,4 +1,11 @@
 package net.vlade.tutorialmod.item;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.vlade.tutorialmod.TutorialMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 }
